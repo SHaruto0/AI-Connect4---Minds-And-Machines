9a99e9931dda16fb08a0384ce42c1c445729e9d7
SHaruto0 <szkharuto0407@gmail.com>
2023-11-30 17:25:48 -0500

    Two Random Number Generator Playing each other
    Two random number generator playing each other and recording the steps into a text files.

--- Connect4Project.py
+++ Connect4Project.py
@@ -1,13 +1,17 @@
+import random
+
 class Connect4:
    def __init__(self):
        self.board = [[0 for _ in range(7)] for _ in range(6)]
        self.current_player = 1
 
-   def drop_piece(self, column):
+   def drop_piece(self, column,result):
        for row in range(5, -1, -1):
            if self.board[row][column] == 0:
+               result.append([self.current_player,(row,column)])
                self.board[row][column] = self.current_player
-               break
+               return True
+       return False
 
    def check_win(self):
        # Check horizontally
@@ -40,21 +44,29 @@ class Connect4:
        for row in self.board:
            print(' '.join(str(cell) for cell in row))
 
-
 def play_game():
-   game = Connect4()
+   f = open("results.txt","w")
+   for i in range(5):
+        game = Connect4()
+        result = []
+        while True:
+            game.print_board()
+            col = random.randint(0,6)
+            #col = int(input("Player " + str(game.current_player) + ", choose a column to drop your piece: "))
 
-   while True:
-       game.print_board()
-       col = int(input("Player " + str(game.current_player) + ", choose a column to drop your piece: "))
-       game.drop_piece(col)
+            if not game.drop_piece(col,result):
+                continue
+            print()
 
-       if game.check_win():
-           game.print_board()
-           print("Player " + str(game.current_player) + " wins!")
-           break
+            if game.check_win():
+                game.print_board()
+                print("Player " + str(game.current_player) + " wins!")
+                result.append(game.current_player)
+                f.write(str(result)+"\n")
+                break
 
-       game.current_player = 2 if game.current_player == 1 else 1
+            game.current_player = 2 if game.current_player == 1 else 1
+   f.close()
 
 
 play_game()
