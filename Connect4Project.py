58ed1db37155b47b9b1aaf3a9d98475b144b0075
SHaruto0 <szkharuto0407@gmail.com>
2023-12-04 17:26:16 -0500

    Connect 4 with AI progress 12/4
    AI simulation working. With list of lists of  values being printed.
    Changed to json file from txt file because data is usually sored in json not txt lmao
    Another file, Connect4Project_v2.py, added for another version of AI

--- Connect4Project.py
+++ Connect4Project.py
@@ -1,4 +1,6 @@
 import random
+import json
+import time
 
 class Connect4:
    def __init__(self):
@@ -45,28 +47,82 @@ class Connect4:
            print(' '.join(str(cell) for cell in row))
 
 def play_game():
-   f = open("results.txt","w")
-   for i in range(5):
+   f = open("results.json","w",encoding="utf-8")
+   r = dict()
+   r[1] = []
+   r[2] = []
+   start_time = time.time()
+   for i in range(50000):
         game = Connect4()
         result = []
         while True:
-            game.print_board()
+            #game.print_board()
             col = random.randint(0,6)
             #col = int(input("Player " + str(game.current_player) + ", choose a column to drop your piece: "))
 
             if not game.drop_piece(col,result):
                 continue
-            print()
+            #print()
 
             if game.check_win():
-                game.print_board()
-                print("Player " + str(game.current_player) + " wins!")
-                result.append(game.current_player)
-                f.write(str(result)+"\n")
+                # game.print_board()
+                # print("Player " + str(game.current_player) + " wins!")
+                r[game.current_player].append(result)
+                print(i)
                 break
+            else:
+                n = 0
+                for row in game.board:
+                    if 0 in row:
+                        n += 1
+                if n == 0:
+                    print("Tie")
+                    break
+
 
             game.current_player = 2 if game.current_player == 1 else 1
+        
+   r = json.dumps(r)
+   f.write(r)
    f.close()
+   print("DONE")
+   print("Time it took " + str(time.time() - start_time))
+
+def read_result():
+    b1 = [[0 for _ in range(7)] for _ in range(6)]
+    b2 = [[0 for _ in range(7)] for _ in range(6)]
+
+    results = open("results.json",encoding="utf-8")
+    results = json.loads(results.read())
+
+    for game in results["1"]:
+        for play in game:
+            if play[0] == 1:
+                row = play[1][0]
+                col = play[1][1]
+                b1[row][col] += 1/len(game)
+            elif play[0] == 2:
+                row = play[1][0]
+                col = play[1][1]
+                b1[row][col] -= 1/len(game)
+
+    for game in results["2"]:
+        for play in game:
+            if play[0] == 1:
+                row = play[1][0]
+                col = play[1][1]
+                b2[row][col] -= 1/len(game)
+            elif play[0] == 2:
+                row = play[1][0]
+                col = play[1][1]
+                b2[row][col] += 1/len(game)
+
+    print(len(results["1"]) + len(results["2"]))
+    print(b1)
+    print(b2)
+        
 
 
-play_game()
+
+
+read_result()
